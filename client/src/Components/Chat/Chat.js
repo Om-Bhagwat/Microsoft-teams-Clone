fdc53407fb8447d017f326a8c1c264266d733d3a
om <ombhagwat141@gmail.com>
2021-07-04 00:31:07 +0530

    Added the UI for Video Calling also The UI for Chatting Although css arrangements for the ui after closing the Chat is required.vscode

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -1,13 +1,16 @@
 import React,{useState,useEffect} from 'react'
 import io from "socket.io-client";
 
+import "../Room/Room.css";
+
 let socket;
 const CONNECTION_PORT = "localhost:8000/"
 
 const Chat=(props)=>{
 
     const {roomID,email} = props;
-    const [connecto,setConnecto] = useState(false);
+    const [connecto,setConnecto] = useState(true);
+    const [open,setOpen] = useState(true);
     console.log(roomID);
 
     const [message,setMessage] = useState("");
@@ -18,14 +21,21 @@ const Chat=(props)=>{
     },[CONNECTION_PORT]);
 
     useEffect(()=>{
+        socket.emit("join_room",roomID);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
+    useEffect(()=>{
         socket.on("recieve_message",(data)=>{
             setMessageList([...messageList,data]);
         })
     },)
 
-    const connectToRoom = () =>{
-        setConnecto(true);
-        socket.emit("join_room",roomID);
+
+    const openChat=(e)=>{
+        e.preventDefault();
+
+        setOpen(!open);
     }
 
     const sendMessage = async () => {
@@ -45,34 +55,43 @@ const Chat=(props)=>{
       console.log(messageList);
 
     return (
-        <div>
+        <>
             {connecto?(
-                <>
-                    <div>
+                
+                <div className="chat-section" >
+                    <h3>Meeting Chat<button onClick={openChat}><i className="medium material-icons">close</i></button></h3>
+                    <div className="chatArea">
                         {messageList.map((val,key)=>{
                             return(
-                                <div id={val.author === email?"YOU":"Other"}>
-                                    {val.author}:{val.message}
+                                <div className="chatheads" id={val.author === email?"YOU":"Other"}>
+                                    <h4>{val.author}</h4>
+                                    <p>{val.message}</p>
+                                    <p>2.12 PM</p>
                                 </div>    
                             )
                         })}
                     </div>
 
-                    <input
+                    <div className="chat-controls">
+                        <input type="text" className="sendChat" onChange={(e)=>setMessage(e.target.value)} placeholder="Write a Message" />
+                        <button onClick={sendMessage}><i className="material-icons">send</i></button>
+                    </div>
+
+                    {/* <input
                         type="text"
                         placeholder="Message..."
                         onChange={(e) => 
                             setMessage(e.target.value)
                         }
                     />
-                    <button onClick={sendMessage}>Send</button>
-                </>
+                    <button onClick={sendMessage}>Send</button> */}
+                </div>
             ):(
                 <>
-                    <button onClick={connectToRoom} >Chat</button>
+                    <button  >Chat</button>
                 </>
             )}
-        </div>
+        </>
     )
 }
 
--- client/src/Components/Room/Room.js
+++ client/src/Components/Room/Room.js
@@ -1,25 +1,25 @@
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import Peer from "simple-peer";
+import {ReactMediaRecorder} from "react-media-recorder";
+
+
 import styled from "styled-components";
 // import socket from "socket.io-client/lib/socket";
+
+import "./Room.css";
 
 
 import Chat from '../Chat/Chat';
 
-const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    height: 100vh;
-    width: 90%;
-    margin: auto;
-    flex-wrap: wrap;
-`;
+
 
 const StyledVideo = styled.video`
-    height: 40%;
-    width: 50%;
+    height: 100%;
+    width: 100%;
 `;
+
+
 
 const Video = (props) => {
     const ref = useRef();
@@ -38,10 +38,11 @@ const Video = (props) => {
 }
 
 
-const videoConstraints = {
-    height: window.innerHeight / 2,
-    width: window.innerWidth / 2
-};
+// const videoConstraints = {
+//     height: window.innerHeight / 2,
+//     width: window.innerWidth / 2
+// };
+
 
 const Room = (props) => {
 
@@ -60,11 +61,10 @@ const Room = (props) => {
     const peersRef = useRef([]);
     const roomID = props.match.params.roomID;
 
-
     useEffect(() => {
         socketRef.current = io.connect("http://localhost:8000/");
         console.log(socketRef.current);
-        navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
             // console.log(localstream.getAudioTracks()[0]);
             userVideo.current.srcObject = stream;
             // console.log(stream.getAudioTracks()[0]);
@@ -73,11 +73,13 @@ const Room = (props) => {
                 const peers = [];
                 users.forEach(userID => {
                     const peer = createPeer(userID, socketRef.current.id,stream);
-                    
+
+
                     peersRef.current.push({
                         peerID: userID,
                         peer,
                     })
+
                     peers.push({
                         peerID:userID,
                         peer,
@@ -86,7 +88,8 @@ const Room = (props) => {
                 setPeers(peers);
             })
 
-            console.log(socketRef);
+
+            console.log(peersRef);
             socketRef.current.on("user joined", payload => {
                 const peer = addPeer(payload.signal, payload.callerID, stream);
                 peersRef.current.push({
@@ -126,8 +129,6 @@ const Room = (props) => {
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
-
     
 
     function createPeer(userToSignal, callerID ,stream) {
@@ -139,6 +140,7 @@ const Room = (props) => {
         peer.on("signal", signal => {
             socketRef.current.emit("sending signal", { userToSignal, callerID, signal,email })
         })
+
 
         return peer;
 
@@ -172,6 +174,8 @@ const Room = (props) => {
     const muteAudio=(e)=>{
         e.preventDefault();
         setAudioMute(!audiomute);
+        // userVideo.current.srcObject.getVideoTracks()[0].contentHint("om");
+        
         userVideo.current.srcObject.getAudioTracks()[0].enabled = !(userVideo.current.srcObject.getAudioTracks()[0].enabled);
     }
 
@@ -180,45 +184,70 @@ const Room = (props) => {
         setVideoMute(!videomute);
         userVideo.current.srcObject.getVideoTracks()[0].enabled = !(userVideo.current.srcObject.getVideoTracks()[0].enabled);
     }
+    
 
-
+    console.log(peers);
     return (
-        <>
-            <Container>
-                <StyledVideo muted ref={userVideo} autoPlay playsInline />
-                <h4>ME</h4>
-                {console.log(peers)}
+        <div className="grid-container">
+
+            <div className="video-screen">
+                <div className="videoBox">
+                    <StyledVideo  ref={userVideo} autoPlay playsInline />
+                    <div className="pName">
+                        You
+                    </div>
+                </div>
                 {peers.map((peer) => {
+                    console.log("joined");
                     return (
-                        <>
+                        <div className="videoBox">
                             <Video key={peer.peerID} peer={peer.peer} />
-                            <h4>{peer.Name}</h4>
-                        </>
+                            <div className="pName">
+                                <h4>{peer.Name}</h4>
+                            </div>
+                        </div>
                     );
                 })}
-            </Container>
-            <button className="btn btn-danger" onClick={leavecall}>HangUp</button>
-            {audiomute?(
-                <>
-                    <button className="btn btn-primary" onClick={muteAudio}>Mute</button>
-                </>
-            ):(
-                <>
-                    <button className="btn btn-primary" onClick={muteAudio}>UnMute</button>
-                </>
-            )}
-            <br></br>
-            {videomute?(
-                <>
-                    <button className="btn btn-primary" onClick={MuteVideo}>Hide Video</button>
-                </>
-            ):(
-                <>
-                    <button className="btn btn-primary" onClick={MuteVideo}>Enable Video</button>
-                </>
-            )}
+
+                <div className="btn-bar">
+                    <div className="sub-btn-bar">
+                        <button  className={`control-btns ${videomute?"toggle-cam-select":"toggle-cam"}`} onClick={MuteVideo}>
+                            {videomute?(
+                                <i className="material-icons">videocam</i>
+                            ):(
+                                <i className="material-icons">videocam_off</i>
+                            )}
+                        </button>
+                        <button className={`control-btns ${audiomute?"toggle-cam-select":"toggle-cam"} `} onClick={muteAudio}>
+                            {audiomute?(
+                                <i className="material-icons end-call">mic_none</i>
+                            ):(
+                                <i className="material-icons end-call">mic_off</i>
+                            )}
+                        </button>
+                        <button className="control-btns hangup" onClick={leavecall}>
+                            <i className="material-icons">call_end</i>
+                        </button>
+                        <button className="control-btns toggle-chat" >
+                            <i className="material-icons">chat</i>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
             <Chat roomID={roomID} email={email} />
-        </>
+            {/* <ReactMediaRecorder
+                video
+                render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+                    <div>
+                        <p>{status}</p>
+                        <button onClick={startRecording}>Start Recording</button>
+                        <button onClick={stopRecording}>Stop Recording</button>
+                        <video src={mediaBlobUrl} controls autoPlay loop />
+                    </div>
+                )}
+            />  */}
+        </div>
     );
 };
 
